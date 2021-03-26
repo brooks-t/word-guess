334b96ff6fc947ba0f6d3c2b71c7118b556e424f
tylercmac <tylercmac@gmail.com>
2021-03-26 14:12:26 -0700

    init and wordguesser func

--- assets/script.js
+++ assets/script.js
@@ -11,23 +11,28 @@ function init() {
     for (let i = 0; i < randWord.length; i++) {
         undScore.push('_');
     }
-    // wordGuesser();
     console.log(randWord);
     console.log(undScore);
+    wordGuesser();
 }
 
 
 
 function wordGuesser(event) {
     var keyPress = event.key;
-    if (randWord.includes(keyPress)) {
-        undScore.replace(randWord.charAt(keyPress), keyPress)
-    } else return;
     document.querySelector('#wordArea').textContent = undScore;
+
+    for (let i = 0; i < randWord.length; i++) {
+        if (randWord.includes(keyPress)) {
+            undScore.replace(randWord[i], keyPress);
+            document.querySelector('#wordArea').textContent = undScore;
+        } else return;
+    }
 
 }
 
 console.log(wordBank);
 
 document.addEventListener('keydown', wordGuesser);
- init();
+
+init();
