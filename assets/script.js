7f18f920e8a013a6bc955aede85992a2ec2f0c22
tylercmac <tylercmac@gmail.com>
2021-03-26 13:53:51 -0700

    initializer, started guesser

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,33 @@
 // page loads
     // loads a random word from wordbank, but it is underscores
 
-let wordBank = ['frog', 'sheep', 'alligator', 'raven', 'tiger', ]
-let randWord = wordBank[Math.floor(Math.random() * wordBank.length)]
+let wordBank = ['frog', 'sheep', 'alligator', 'raven', 'tiger', 'platypus']
+let randWord = wordBank[Math.floor(Math.random() * wordBank.length)].split('');
 let undScore = []
 
 
 
 function init() {
-    wordGuesser();
+    for (let i = 0; i < randWord.length; i++) {
+        undScore.push('_');
+    }
+    // wordGuesser();
+    console.log(randWord);
+    console.log(undScore);
 }
+
 
 
 function wordGuesser(event) {
     var keyPress = event.key;
+    if (randWord.includes(keyPress)) {
+        undScore.replace(randWord.charAt(keyPress), keyPress)
+    } else return;
     document.querySelector('#wordArea').textContent = undScore;
 
-    for (let i = 0; i < randWord.length; i++) {
-        undScore.push('_');
-        if (randWord.includes(keyPress)) {
-            undScore.replace(randWord.charAt(keyPress), keyPress)
-        } else return;
-    }
 }
 
 console.log(wordBank);
 
 document.addEventListener('keydown', wordGuesser);
-
-init();
+ init();
